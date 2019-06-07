9890ad1b2dc282d61868d0dd017065bd444471a3
Jonathan King <imjonathanking@gmail.com>
2019-06-07 14:31:04 -0400

    Typo in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 
 if(process.env.JAWSDB_URL){
-  var connection = mysql.createConnection(process.env.JAWDB_URL);
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else{
   var connection = mysql.createConnection({
