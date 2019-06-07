1ae6867c765d5867410a5c7bab489c7cac30cbb6
Jonathan King <imjonathanking@gmail.com>
2019-06-07 13:57:17 -0400

    Fixed config file

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-if(process.env.JAWDB_URL){
+if(process.env.JAWSDB_URL){
   var connection = mysql.createConnection(process.env.JAWDB_URL);
 }
 else{
